f76e2df000bc1a29a1473770f4fc3dfbd0a8e70d
bhelms2132 <bhelms2132@gmail.com>
2019-11-17 20:56:07 -0500

    odject array of suggested friends created and able to export

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,18 +1,20 @@
 // Dependencies
 var express = require("express");
 var path = require("path");
-var nodemon = require("nodemon");
+var bodyParser = require("body-parser");
+//var nodemon = require("nodemon");
 var app = express();
 // Sets an initial port.
-const PORT = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
-app.use(express.static('app/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static("app/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 // Router
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 // Listner
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(port, function() {
+  console.log("App listening on PORT: " + port);
 });
